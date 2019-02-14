d2c66197c6e75318eeeaf647ead7c36ed6355a0b
Roberto García <roberto@rhizomik.net>
2019-02-14 17:21:58 +0100

    Add service to manage security controls for owners or public

---
+++ src/main/java/net/rhizomik/rhizomer/service/SecurityController.java
@@ -0,0 +1,32 @@
+package net.rhizomik.rhizomer.service;
+
+import net.rhizomik.rhizomer.model.Admin;
+import net.rhizomik.rhizomer.model.Dataset;
+import org.springframework.security.access.AuthorizationServiceException;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by http://rhizomik.net/~roberto/
+ */
+@Service
+public class SecurityController {
+
+    public void checkPublicOrOwner(Dataset dataset, Authentication auth)
+        throws AuthorizationServiceException{
+        if (dataset.isPublic())
+            return;
+        checkOwner(dataset, auth);
+    }
+
+    public void checkOwner(Dataset dataset, Authentication auth)
+        throws AuthorizationServiceException {
+        if (auth == null)
+            throw new AuthorizationServiceException("Not authorized to access dataset "+ dataset.getId());
+        if (auth.isAuthenticated() &&
+            !(auth.getPrincipal() instanceof Admin) &&
+            !dataset.getOwner().getUsername().equals(auth.getName()))
+            throw new AuthorizationServiceException("User " + auth.getName() +
+                " not authorized to access dataset "+ dataset.getId());
+    }
+}
